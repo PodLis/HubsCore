11676c7cdead565f3010989c1aa732f905dd90b8
Rosenboum <RosenZM@yandex.ru>
2020-01-12 21:06:24 +0700

    Initial commit

---
+++ src/main/java/ru/hubsmc/hubscore/HubsCore.java
@@ -0,0 +1,17 @@
+package ru.hubsmc.hubscore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HubsCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
